d97a42921e31d9d2ea15581fc5b1ab7d8fbcc361
alan de los santos dominguez <asantos@cxc.com.mx>
2025-02-27 18:37:51 -0700

    prueba 7

--- src/main/java/com/example/spring_boot/controllers/SimpleController.java
+++ src/main/java/com/example/spring_boot/controllers/SimpleController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class SimpleController {
     @GetMapping("/status")
     public String getStatus() {
-        return "OK simoc";
+        return "OK simon";
     }
 }
