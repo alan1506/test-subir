7b1eeeb7f9a7a9fec0734db97dae99c8922ca5d2
alan de los santos dominguez <asantos@cxc.com.mx>
2025-04-08 16:48:24 -0700

    prueba 30

--- src/main/java/com/example/spring_boot/controllers/SimpleController.java
+++ src/main/java/com/example/spring_boot/controllers/SimpleController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SimpleController {
     @GetMapping("/status")
     public String getStatus() {
-        return "OK prueba rollout latets29999";
+        return "OK prueba rollout latets2999966";
     }
 
     @GetMapping("/health")
