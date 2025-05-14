eb55bb5affdfc49d791b30b411509c0412ca395b
alan de los santos dominguez <asantos@cxc.com.mx>
2025-05-13 21:30:12 -0700

    sonar

--- src/main/java/com/example/spring_boot/controllers/SimpleController.java
+++ src/main/java/com/example/spring_boot/controllers/SimpleController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SimpleController {
     @GetMapping("/status")
     public String getStatus() {
-        return "OK prueba rollout latets2999966";
+        return "OK prueba rollout sonar";
     }
 
     @GetMapping("/health")
